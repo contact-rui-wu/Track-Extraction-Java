adb2d987984c99dfb0c28457be1a0c74fd43ec7a
Rui Wu <contact.rui.wu@gmail.com>
2016-05-25 10:04:28 -0400

    Writing some tests

--- src/TrackExtractionJava/Test_Rui.java
+++ src/TrackExtractionJava/Test_Rui.java
@@ -4,18 +4,20 @@
 
 package TrackExtractionJava;
 
-// TODO what to import?
+// what to import?
 
 public class Test_Rui {
 	// write each test as a void method so that don't have to write a lot in main
 
+	/**
+	 * Computes ddt for one frame
+	 */
 	public static void test_ddt() {
-		/** compute ddt for one frame
-		 * 3 methods: forward, backward, central
-		 * each needs begin/end messages
-		 * for now just provide it with 3 images
-		 * (need to study Natalie's data structure to know where to grab previous/next frames)
-		 */
+		// 3 methods: forward, backward, central
+		// each needs begin/end messages
+		// for now just provide it with 3 images
+		// (need to study Natalie's data structure to know where to grab previous/next frames)
+		
 	}
 	
 	public static void main(String[] args) {
