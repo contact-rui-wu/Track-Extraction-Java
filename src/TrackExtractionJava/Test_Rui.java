a4f20c704fda0cafd4897f55452aa5c0046387ea
Rui Wu <contact.rui.wu@gmail.com>
2016-05-24 20:00:52 -0400

    Clock out: added Test_Rui

---
+++ src/TrackExtractionJava/Test_Rui.java
@@ -0,0 +1,27 @@
+/**
+ * Sandbox for the Bermanator
+ */
+
+package TrackExtractionJava;
+
+// TODO what to import?
+
+public class Test_Rui {
+	// write each test as a void method so that don't have to write a lot in main
+
+	public static void test_ddt() {
+		/** compute ddt for one frame
+		 * 3 methods: forward, backward, central
+		 * each needs begin/end messages
+		 * for now just provide it with 3 images
+		 * (need to study Natalie's data structure to know where to grab previous/next frames)
+		 */
+	}
+	
+	public static void main(String[] args) {
+		// put the testing methods here
+		
+		//test_ddt();
+	}
+
+}
