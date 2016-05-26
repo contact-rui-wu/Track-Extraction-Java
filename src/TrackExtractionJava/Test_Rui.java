7ac9edd24c38065de89cfa9ebaebf5cf34130995
Rui Wu <contact.rui.wu@gmail.com>
2016-05-26 18:20:37 -0400

    Clock out: adapting tests to handle ImageJ classes

--- src/TrackExtractionJava/Test_Rui.java
+++ src/TrackExtractionJava/Test_Rui.java
@@ -4,8 +4,10 @@
 
 package TrackExtractionJava;
 
-// what to import?
-// - ImageJ ImageProcessor stuff
+import ij.process.ByteProcessor;
+import ij.process.FloatProcessor;
+import ij.ImageStack;
+import ij.ImagePlus;
 
 public class Test_Rui {
 
@@ -13,79 +15,120 @@ public class Test_Rui {
 		// put the testing methods here
 		// uncomment when a test is ready to run
 		
-		//test_pointGraber();
+		/**
+		test_pointGraber();
+		*/
 		
-		//test_ddt();
+		/**
+		 * prepare images for the test
+		test_ddt();
+		 */
 		
-		//test_scribe();
+		/**
+		test_scribe();
+		*/
 	}
 	
 	// write each test as a void method so that don't have to write a lot in main
 
 	/**
-	 * Grabs the correct points from a track depending on chosen deriv method
+	 * Grabs the correct points at/near time t from a track depending on chosen deriv method
 	 * @param track track we're working on
-	 * @param timeStamp frame we're working on
-	 * @param derivMethod forward/backward/central
+	 * @param t frame we're working on
+	 * @param derivMethod forward(1)/backward(2)/central(3)
 	 * @return 2 or 3 points depending on derivMethod
 	 */
-	public static void test_pointGraber() {
-		// actually not void
+	public static void test_grabPoints() {
+		// placeholder
+		ImageStack track;
+		int t;
+		int derivMethod=1; // change manually: 1 for forward, 2 for backward, 3 for central
+		
+		// prepare empty result image stack
+		ImageStack theseIm = new ImageStack();
+		
+		// for test purpose: visualization
+
 	}
 	
 	/**
-	 * Computes ddt for one point
-	 * @param derivMethod forward/backward/central
-	 * @param thisIm an ImageJ ImageProcessor object (I think); always needed
-	 * @param lastIm needed for backward and central
-	 * @param nextIm needed for forward and central
-	 * @return ddt value matrix, doesn't have to be ImageProcessor (right?)
+	 * Given 2 points, compute ddt between them
+	 * @param point1 earlier point
+	 * @param point2 later point
+	 * @param dt time increment between them
+	 * @return ddt image, 8 bit gray scale
 	 */
-	public static void test_ddt(int derivMethod, ImageProcessor thisIm, ImageProcessor lastIm, ImageProcessor nextIm) {
-		// actually not void
-		// 3 methods: forward, backward, central
-		// assume correct roi TODO think about how to update roi (check getCombinedBounds)
-		// assume same threshold TODO later need to account for changed threshold
+	public static void test_ddt() {
+		// placeholder
+		ByteProcessor point1;
+		ByteProcessor point2;
+		int dt=1; // change manually: 1 for forward/backward, 2 for central
+		
 		// assume 8-bit gray scale
+		// Q: do I still need to deal with threshold?
 		
-		// load frames
-		// suppose we have width, height, *Im.frameNum, *Im.getPixVal(i,j)
+		// TODO get correct roi
+		// check getCombinedBounds
+		// for now: assume they overlap, use point1 dimension
+		int width = point1.getWidth();
+		int height = point1.getHeight();
 		
-		// default: assume 1 frame increment between frames TODO check FrameLoader
-		int dt = 1;
+		// prepare empty result image
+		FloatProcessor ddtIm = new FloatProcessor(width, height);
 		
-		// forward
+		// fill ddtIm
 		for(int i=0; i<width; i++) {
 			for(int j=0; j<height; j++) {
-				int PixDiff = nextIm.getPixVal(i,j)-thisIm.getPixVal(i,j);
-				float ddt = PixDiff/dt;
+				int pixDiff = point2.getPixel(i,j)-point1.getPixel(i,j);
+				float ddt = ((pixDiff+255)/2)/dt;
+				ddtIm.setf(i,j,ddt);
 			}
 		}
 		
-		// backward
-		for(int i=0; i<width; i++) {
-			for(int j=0; j<height; j++) {
-				int PixDiff = thisIm.getPixVal(i,j)-lastIm.getPixVal(i,j);
-				float ddt = PixDiff/dt;
-			}
+		/**
+		 * scratch: ddt actually doesn't need 3 images, so it dosen't need to know which method either
+		// fill ddtIm
+		switch(derivMethod) {
+			case 1 : // forward
+				for(int i=0; i<width; i++) {
+					for(int j=0; j<height; j++) {
+						int pixDiff = nextIm.getPixel(i,j)-thisIm.getPixel(i,j);
+						float ddt = pixDiff/dt;
+						// deal with -ve values (Natalie used color)
+						ddtIm.setf(i,j,ddt);
+					}
+				}
+			case 2 : // backward
+				for(int i=0; i<width; i++) {
+					for(int j=0; j<height; j++) {
+						int PixDiff = thisIm.getPixel(i,j)-prevIm.getPixel(i,j);
+						float ddt = PixDiff/dt;
+						ddtIm.setf(i,j,ddt);
+					}
+				}
+			case 3 : // central
+				for(int i=0; i<width; i++) {
+					for(int j=0; j<height; j++) {
+						int PixDiff = nextIm.getPixel(i,j)-prevIm.getPixel(i,j);
+						float ddt = PixDiff/(2*dt);
+						ddtIm.setf(i,j,ddt);
+					}
+				}
 		}
+		*/
 		
-		// central
-		for(int i=0; i<width; i++) {
-			for(int j=0; j<height; j++) {
-				int PixDiff = nextIm.getPixVal(i,j)-lastIm.getPixVal(i,j);
-				float ddt = PixDiff/(2*dt);
-			}
-		}
+		// for test purpose: visualization
+		ImagePlus ddtImPlus = new ImagePlus("ddt image", ddtIm);
+		ddtImPlus.show();
 		
 	}
 	
 	/**
 	 * Adds ddt value to the corresponding point
 	 * @param thisPoint point to add to
-	 * @param ddtMtx ddt value matrix computed by test_ddt
+	 * @param ddtIm ddt value matrix computed by test_ddt
 	 */
-	public static void scribe() {
+	public static void test_scribe() {
 		
 	}
 
