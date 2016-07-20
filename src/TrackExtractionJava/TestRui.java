907cbb25ff142a7375a0f04fb7f40f9ffeb4f94d
Rui Wu <contact.rui.wu@gmail.com>
2016-07-20 18:25:08 -0400

    Organizing codes

--- src/TrackExtractionJava/TestRui.java
+++ src/TrackExtractionJava/TestRui.java
@@ -12,42 +12,6 @@ public class TestRui {
 
 	}
 	
-	public static void test_dataVersion0Handling() {
-		///// do extraction without saving /////
-		ImageJ ij = new ImageJ();
-		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/data_version_handling/sampleShortExp/";
-		String fname = "sampleShortExp_copy";
-		// set parameters
-		ProcessingParameters prParams = new ProcessingParameters();
-		prParams.diagnosticIm = false;
-		prParams.showMagEx = false;
-		prParams.saveMagEx = false;
-		prParams.doFitting = true;
-		prParams.showFitEx = true;
-		prParams.saveFitEx = true;
-		prParams.saveErrors = false;
-		prParams.saveSysOutToFile = false;
-		ExtractionParameters extrParams = new ExtractionParameters();
-		extrParams.subset = true;
-		extrParams.startFrame = 1;
-		extrParams.endFrame = 1000;
-		FittingParameters fitParams = new FittingParameters();
-		fitParams.storeEnergies = false;
-		// prepare processor
-		Experiment_Processor ep = new Experiment_Processor();
-		ep.runningFromMain = true;
-		ep.prParams = prParams;
-		ep.extrParams = extrParams;
-		ep.fitParams = fitParams;
-		// run processor
-		ep.run(dir+fname+".mmf");
-		// fetch experiment
-		Experiment ex = ep.ex;
-		///// save magEx to disk /////
-		
-		///// load magEx from disk /////
-	}
-	
 	public static void test_extraction() {
 		ImageJ ij = new ImageJ();
 		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/sampleShortExp/";
