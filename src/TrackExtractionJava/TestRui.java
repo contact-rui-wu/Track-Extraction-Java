e2fad7c45a562d69f777557c12ff41d0a19f13b0
Rui Wu <contact.rui.wu@gmail.com>
2016-07-20 14:03:55 -0400

    Started on data version handling

--- src/TrackExtractionJava/TestRui.java
+++ src/TrackExtractionJava/TestRui.java
@@ -6,14 +6,17 @@ public class TestRui {
 
 	public static void main(String[] args) {
 		
+		//test_dataVersion0Handling();
+		
 		test_extraction();
 
 	}
 	
-	public static void test_extraction() {
+	public static void test_dataVersion0Handling() {
+		///// do extraction without saving /////
 		ImageJ ij = new ImageJ();
-		TicToc timer = new TicToc();
-
+		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/data_version_handling/";
+		String mmfname = "sampleShortExp_copy.mmf";
 		// set parameters
 		ProcessingParameters prParams = new ProcessingParameters();
 		prParams.doFitting = true;
@@ -25,6 +28,39 @@ public class TestRui {
 		extrParams.subset = true;
 		extrParams.startFrame = 1;
 		extrParams.endFrame = 1000;
+		FittingParameters fitParams = new FittingParameters();
+		fitParams.storeEnergies = false;
+		// run extraction
+		Experiment_Processor ep = new Experiment_Processor();
+		ep.runningFromMain = true;
+		ep.prParams = prParams;
+		ep.extrParams = extrParams;
+		ep.fitParams = fitParams;
+		ep.run(dir+mmfname);
+		
+		///// save experiment to disk /////
+		
+		///// load experiment from disk /////
+	}
+	
+	public static void test_extraction() {
+		ImageJ ij = new ImageJ();
+		
+		// set parameters
+		ProcessingParameters prParams = new ProcessingParameters();
+		prParams.showMagEx = false;
+		prParams.saveMagEx = false;
+		prParams.doFitting = true;
+		prParams.showFitEx = true;
+		prParams.saveFitEx = false;
+		prParams.saveErrors = false;
+		prParams.saveSysOutToFile = false;
+		ExtractionParameters extrParams = new ExtractionParameters();
+		extrParams.subset = true;
+		extrParams.startFrame = 1;
+		extrParams.endFrame = 1000;
+		FittingParameters fitParams = new FittingParameters();
+		fitParams.storeEnergies = false;
 
 		String path = "/home/data/rw1679/Documents/Gershow_lab_local/sampleShortExp_copy.mmf";
 		Experiment_Processor ep = new Experiment_Processor();
@@ -33,10 +69,9 @@ public class TestRui {
 
 		ep.prParams = prParams;
 		ep.extrParams = extrParams;
+		ep.fitParams = fitParams;
 
-		timer.tic();
 		ep.run(path);
-		System.out.println("Runtime: "+timer.toc()/1000+" seconds");
 	}
 
 }
