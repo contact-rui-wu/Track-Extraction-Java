28f47cbefe536f7dfa5461bf2b7a593e3add8403
Rui Wu <contact.rui.wu@gmail.com>
2016-11-29 15:06:40 -0500

    making git happy

--- src/TrackExtractionJava/TestRui.java
+++ src/TrackExtractionJava/TestRui.java
@@ -5,19 +5,25 @@ import ij.ImageJ;
 public class TestRui {
 
 	public static void main(String[] args) {
-				
-		test_pipeline();
+		
+		test_isDebugWorking();
+		
+		//test_pipeline();
 
+	}
+	
+	public static void test_isDebugWorking() {
+		System.out.println("Is debug working?");
 	}
 	
 	public static void test_pipeline() {
 		ImageJ ij = new ImageJ();
-		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/sampleShortExp/";
+		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/Java/";
 		String mmfname = "sampleShortExp_copy.mmf";
 		// set parameters
 		ProcessingParameters prParams = new ProcessingParameters();
 		prParams.diagnosticIm = false;
-		prParams.showMagEx = true;
+		prParams.showMagEx = false;
 		prParams.saveMagEx = true;
 		prParams.doFitting = true;
 		prParams.showFitEx = true;
@@ -25,14 +31,14 @@ public class TestRui {
 		prParams.saveErrors = false;
 		prParams.saveSysOutToFile = false;
 		ExtractionParameters extrParams = new ExtractionParameters();
-		extrParams.subset = true;
+		//extrParams.subset = true;
 		extrParams.startFrame = 1;
 		extrParams.endFrame = 1000;
 		FittingParameters fitParams = new FittingParameters();
 		fitParams.storeEnergies = false;
 		// prepare processor
 		Experiment_Processor ep = new Experiment_Processor();
-		ep.runningFromMain = true;
+		//ep.runningFromMain = true;
 		ep.prParams = prParams;
 		ep.extrParams = extrParams;
 		ep.fitParams = fitParams;
