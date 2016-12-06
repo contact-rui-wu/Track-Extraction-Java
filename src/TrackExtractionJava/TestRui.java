9a752725db26d9279416764c272a520e1276bd90
Rui Wu <contact.rui.wu@gmail.com>
2016-12-06 17:05:03 -0500

    Updated pipeline test wrt to Natalie's and Marc's codes

--- src/TrackExtractionJava/TestRui.java
+++ src/TrackExtractionJava/TestRui.java
@@ -17,9 +17,14 @@ public class TestRui {
 	}
 	
 	public static void test_pipeline() {
+		// set timer
+		TicToc timer = new TicToc();
+		timer.tic();
+		// prepare file names
 		ImageJ ij = new ImageJ();
-		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/Java/";
-		String mmfname = "sampleShortExp_copy.mmf";
+		String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/Java";
+		String dataID = "sampleLongExp_copy";
+		String mmfName = dataID+".mmf";
 		// set parameters
 		ProcessingParameters prParams = new ProcessingParameters();
 		prParams.diagnosticIm = false;
@@ -32,8 +37,8 @@ public class TestRui {
 		prParams.saveSysOutToFile = false;
 		ExtractionParameters extrParams = new ExtractionParameters();
 		//extrParams.subset = true; // deprecated
-		extrParams.startFrame = 1;
-		extrParams.endFrame = 1000;
+		//extrParams.startFrame = 1;
+		//extrParams.endFrame = 10000;
 		FittingParameters fitParams = new FittingParameters();
 		fitParams.storeEnergies = false;
 		// prepare processor
@@ -43,7 +48,10 @@ public class TestRui {
 		ep.extrParams = extrParams;
 		ep.fitParams = fitParams;
 		// run processor
-		ep.run(dir+mmfname);
+		// syntax: run(String srcFileName, String dstDir, String dstName)
+		ep.run(dir+"/"+mmfName,dir,dir+"/"+dataID);
+		// stop timer
+		System.out.println("Pipeline time: "+timer.toc()/1000+"s");
 	}
 
 }
