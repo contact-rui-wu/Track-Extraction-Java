9e79c1bbed114f3873af43e91fdd527a62403a86
Rui Wu <contact.rui.wu@gmail.com>
2017-03-06 12:41:41 -0500

    added multi-platform util methods in TestRui

--- src/TrackExtractionJava/TestRui.java
+++ src/TrackExtractionJava/TestRui.java
@@ -5,12 +5,28 @@ import ij.ImageJ;
 import java.io.File;
 
 public class TestRui {
+	
+	private static String OS = null;
+	private static String getOSName() {
+		if (OS==null) {
+			OS = System.getProperty("os.name");
+		}
+		return OS;
+	}
+	private static boolean isWindows() {
+		return getOSName().startsWith("Windows");
+	}
+	private static boolean isLinux() {
+		return getOSName().startsWith("Linux");
+	}
 
 	public static void main(String[] args) {
 		
 		test_pipeline();
 		
-		//test_isDebugWorking();
+//		test_isDebugWorking();
+		
+//		test_checkOS();
 
 	}
 	
@@ -23,44 +39,40 @@ public class TestRui {
 		ImageJ ij = new ImageJ();
 		
 		// prepare file paths
-		//String dir = "/home/data/rw1679/Documents/Gershow_lab_local/pipeline/Java";
-		//String dataID = "sampleLongExp_copy";
-		//String dir = "D:\\Life Matters\\Research\\with Marc Gershow\\data\\code-test\\";
-		//String dataID = "sampleExp-copy";
-		//String mmfName = dataID+".mmf";
 		String exID = "sampleExp-copy";
-		String srcDir = "D:\\Life Matters\\Research\\with Marc Gershow\\data\\code-test\\"; // on windows
-		String srcPath = srcDir+exID+".mmf";
-		String dstDir = srcDir+exID+"_master"+File.separator; // add branch label
-		//String dstPath = dstDir+exID;
-		
+		String srcDir = "";
+		if (isWindows()) {
+			srcDir = "D:\\Life Matters\\Research\\with Marc Gershow\\data\\code-test\\";
+		} else if (isLinux()) {
+			srcDir = "/home/data/rw1679/Documents/gershow-lab-local/data/java-pipeline/";
+		}
+		String srcPath = srcDir + exID + ".mmf";
+		String dstDir = srcDir + exID + "_master" + File.separator; // add branch label
+
 		// set parameters
 		ProcessingParameters prParams = new ProcessingParameters();
 		prParams.diagnosticIm = false;
 		prParams.showMagEx = true;
-		prParams.saveMagEx = true;
+		prParams.saveMagEx = false;
 		prParams.doFitting = false;
 		prParams.showFitEx = false;
 		prParams.saveFitEx = false;
 		prParams.saveErrors = false;
 		prParams.saveSysOutToFile = false;
 		ExtractionParameters extrParams = new ExtractionParameters();
-		//extrParams.subset = true; // deprecated
-		//extrParams.startFrame = 1; // default=1
-		//extrParams.endFrame = 10000; // default=Integer.MAX_VALUE
+//		extrParams.startFrame = 1; // default=1
+		extrParams.endFrame = 1000; // default=Integer.MAX_VALUE
 		FittingParameters fitParams = new FittingParameters();
 		fitParams.storeEnergies = false;
 		
 		// prepare processor
 		Experiment_Processor ep = new Experiment_Processor();
-		//ep.runningFromMain = true; // deprecated
 		ep.prParams = prParams;
 		ep.extrParams = extrParams;
 		ep.fitParams = fitParams;
 		
 		// run extraction pipeline
 		// syntax: run(String srcFileName, String dstDir, String dstName)
-		//ep.run(dir+"/"+mmfName,dir,dir+"/"+dataID);
 		ep.run(srcPath,dstDir,exID);
 		
 		// stop timer
@@ -69,6 +81,20 @@ public class TestRui {
 
 	public static void test_isDebugWorking() {
 		System.out.println("Is debug working on master?");
+	}
+	
+	public static void test_checkOS() {
+//		System.out.println(System.getProperty("os.name"));
+		if (isWindows()) {
+			System.out.println("OS is Windows");
+		} else {
+			System.out.println("OS is not Windows");
+		}
+		if (isLinux()) {
+			System.out.println("OS is Linux");
+		} else {
+			System.out.println("OS is not Linux");
+		}
 	}
 
 }
